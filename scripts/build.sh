e5cc535052fae7b2b1ba971dbc3e361eb159ee67
Phil Eaton <phil@eatonphil.com>
2022-10-04 19:14:45 +0000

    Add dedicated build script via docker for glibc alignment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,5 @@
-docker run -v $(pwd):/wrk -w /wrk --entrypoint bash node -c "npm install && npm pack"
+docker run -v $(pwd):/wrk -w /wrk --entrypoint bash node -c "
+npm config set cache /tmp --global
+npm install
+npm pack
+"
